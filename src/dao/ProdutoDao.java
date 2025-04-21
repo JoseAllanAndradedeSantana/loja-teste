4130bb07c2f1e6662968ff9f011d013436f4e358
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2025-04-21 10:58:26 -0300

    alterando produtoDao

--- src/dao/ProdutoDao.java
+++ src/dao/ProdutoDao.java
@@ -23,14 +23,14 @@ public class ProdutoDao {
             sql
                     .append("INSERT INTO produto (nome,preco,ativo) ")
                     .append("VALUES ( ? ,?, ?) ");
-
-            PreparedStatement ps = connectionFactory.createConexao().prepareStatement(sql.toString());
+            Connection conexao = connectionFactory.createConexao();
+            PreparedStatement ps = conexao.prepareStatement(sql.toString());
             ps.setString(i++,produto.getNome());
             ps.setDouble(i++,produto.getPreco());
             ps.setBoolean(i, STATUS_ATIVO);
             ps.execute();
             ps.close();
-            connectionFactory.closedConexao();
+            conexao.close();
         } catch (SQLException e) {
             throw new RuntimeException("Erro ao salvar Produto -> " + e.getMessage());
         }
@@ -39,18 +39,16 @@ public class ProdutoDao {
     public void excluir(String codigo){
         try {
             int i = 1;
-            Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/loja_allan","root","1234");
             StringBuilder sql = new StringBuilder();
-
             sql
                     .append("DELETE FROM produto ")
                     .append("WHERE codigo = ? ");
-
+            Connection conexao = connectionFactory.createConexao();
             PreparedStatement ps = conexao.prepareStatement(sql.toString());
             ps.setString(i,codigo);
             ps.executeUpdate();
             ps.close();
-            conexao.close();
+            conexao.close();;
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage());
         }
@@ -59,9 +57,9 @@ public class ProdutoDao {
     public List<Produto> buscarTodosProdutos(){
         List<Produto> produtos = new ArrayList<Produto>();
         try{
-            Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/loja_allan","root","1234");
             StringBuilder sql = new StringBuilder();
             sql.append("SELECT codigo,nome, preco FROM produto");
+            Connection conexao = connectionFactory.createConexao();
             PreparedStatement ps = conexao.prepareStatement(sql.toString());
             ResultSet resultSet = ps.executeQuery();
             while (resultSet.next()){
@@ -87,14 +85,12 @@ public class ProdutoDao {
     public void atualizar(String codigo,Produto produto){
         try {
             int i = 1;
-            Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/loja_allan","root","1234");
             StringBuilder sql = new StringBuilder();
-
             sql
                     .append("UPDATE produto ")
                     .append("SET codigo = ?, nome = ? , preco = ?, ativo = ? ")
                     .append("WHERE codigo = ? ");
-
+            Connection conexao = connectionFactory.createConexao();
             PreparedStatement ps = conexao.prepareStatement(sql.toString());
             ps.setLong(i++,produto.getCodigo());
             ps.setString(i++, produto.getNome());
@@ -113,9 +109,9 @@ public class ProdutoDao {
     public Produto buscarProdutoPorCodigo(String codigo) {
         Produto produto =null;
         try{
-            Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/loja_allan","root","1234");
             StringBuilder sql = new StringBuilder();
             sql.append("SELECT codigo,nome, preco FROM produto WHERE codigo = ?");
+            Connection conexao = connectionFactory.createConexao();
             PreparedStatement ps = conexao.prepareStatement(sql.toString());
             ps.setString(1,codigo);
             ResultSet resultSet = ps.executeQuery();
@@ -123,10 +119,7 @@ public class ProdutoDao {
                 long id = resultSet.getLong("codigo");
                 String nomeProduto = resultSet.getString("nome");
                 Double preco = resultSet.getDouble("preco");
-
                 produto = new Produto(id,nomeProduto,preco,false);
-
-
             }
             ps.close();
             conexao.close();
@@ -138,9 +131,12 @@ public class ProdutoDao {
 
     public boolean isProdutoComMesmoNome(Produto produto) throws SQLException {
         String sql = "SELECT nome FROM produto WHERE nome = ? ";
-        PreparedStatement ps = connectionFactory.createConexao().prepareStatement(sql);
+        Connection conexao = connectionFactory.createConexao();
+        PreparedStatement ps = conexao.prepareStatement(sql);
         ps.setString(1, produto.getNome());
         ResultSet resultSet = ps.executeQuery();
+        ps.close();
+        conexao.close();
         if(resultSet.next()){
             return true;
         };
