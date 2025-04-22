64efcc1c090d9c023b4b17ab30c6216854c511c3
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2025-04-22 10:17:59 -0300

    iniciando UsuarioDao com funcionalidade de incluir

---
+++ src/dao/UsuarioDao.java
@@ -0,0 +1,30 @@
+package dao;
+
+import domain.Usuario;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UsuarioDao {
+
+    private ConnectionFactory connectionFactory;
+
+    public void incluir(Usuario usuario){
+        try {
+            int i = 1;
+            String sqlQuery = "INSERT INTO usuario (nome,email,senha) VALUES (? , ?, ? )";
+            Connection conexao = connectionFactory.createConexao();
+            PreparedStatement preparedStatement = conexao.prepareStatement(sqlQuery);
+            preparedStatement.setString(i++,usuario.getNome());
+            preparedStatement.setString(i++,usuario.getEmail());
+            preparedStatement.setString(i,usuario.getSenha());
+            preparedStatement.execute();
+            preparedStatement.close();
+            conexao.close();
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao salvar Usuario -> " + e.getMessage());
+        }
+
+    }
+}
