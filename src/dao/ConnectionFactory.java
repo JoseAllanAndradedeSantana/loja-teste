2fe6182c827407a33d07d5ccbcb3cbfe8416f525
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2025-04-22 20:27:52 -0300

    adicionando no projeto hikariCP para conexoes ao bd

--- src/dao/ConnectionFactory.java
+++ src/dao/ConnectionFactory.java
@@ -1,15 +1,30 @@
 package dao;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    public Connection createConexao(){
+    private HikariDataSource hikariDataSource;
 
+    public ConnectionFactory(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/loja_allan");
+        hikariConfig.setUsername("root");
+        hikariConfig.setPassword("1234");
+        hikariConfig.setMinimumIdle(3);
+        hikariConfig.setMaximumPoolSize(5);
+        hikariDataSource = new HikariDataSource(hikariConfig);
+    }
+
+    public Connection createConexao(){
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/loja_allan","root","1234");
+            return hikariDataSource.getConnection();
+            //return DriverManager.getConnection("jdbc:mysql://localhost:3306/loja_allan","root","1234");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
