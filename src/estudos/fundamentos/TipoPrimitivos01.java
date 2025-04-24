251f22c06357d36c1afa1417c3953f680924aecc
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2025-04-24 12:40:57 -0300

    tipos primitivos em java

---
+++ src/estudos/fundamentos/TipoPrimitivos01.java
@@ -0,0 +1,25 @@
+package estudos.fundamentos;
+
+public class TipoPrimitivos01 {
+
+    public static void main(String[] args){
+        int idade = 35;
+        double salario = 3500D;
+        float salarioDev = 4000f;
+        long identidicacao = 28;
+        boolean isDev = true;
+        byte b = 27;
+        char letra = 'A';
+        short numero = 5;
+
+        System.out.println(idade);
+        System.out.println(salario);
+        System.out.println(salarioDev);
+        System.out.println(identidicacao);
+        System.out.println(isDev);
+        System.out.println(b);
+        System.out.println(letra);
+        System.out.println(numero);
+
+    }
+}
