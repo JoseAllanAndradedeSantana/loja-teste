7750e2356e1cb2777925fa75233c45f79dce4f90
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2025-04-24 12:41:28 -0300

    tipos primitivos casting em java

---
+++ src/estudos/fundamentos/TiposPrimitivosCasting.java
@@ -0,0 +1,8 @@
+package estudos.fundamentos;
+
+public class TiposPrimitivosCasting {
+    public static void main(String[] args){
+        int idade = (int) 100L;
+        long preco = (long) 125.33;
+    }
+}
