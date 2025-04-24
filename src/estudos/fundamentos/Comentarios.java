3c8f4015f6caf4e2306254bdd52ba187942c149d
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2025-04-24 12:39:35 -0300

    comentarios java

---
+++ src/estudos/fundamentos/Comentarios.java
@@ -0,0 +1,15 @@
+package estudos.fundamentos;
+/**
+ * <h1>teste documentacao</h1>
+* comentatios doc
+*
+* */
+public class Comentarios {
+    // comentarios simples
+
+  /*
+     comentarios de multiplas linhas
+   */
+
+
+}
