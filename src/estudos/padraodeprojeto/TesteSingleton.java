36ae169f1e5896b9f4de0efc634aa975c96f15ee
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2025-05-01 22:24:31 -0300

    estudando padrao singleton

--- src/estudos/padraodeprojeto/TesteSingleton.java
+++ src/estudos/padraodeprojeto/TesteSingleton.java
@@ -2,9 +2,22 @@ package estudos.padraodeprojeto;
 
 public class TesteSingleton {
     public static void main(String[] args) {
+        System.out.println("TESTANDO INSTANCIA LAZY");
         SingletonLazy lazy = SingletonLazy.getInstance();
         System.out.println(lazy);
         lazy = SingletonLazy.getInstance();
         System.out.println(lazy);
+
+        System.out.println("TESTANDO INSTANCIA EAGER");
+        SingletonEager eager = SingletonEager.getInstance();
+        System.out.println(eager);
+        eager = SingletonEager.getInstance();
+        System.out.println(eager);
+
+        System.out.println("TESTANDO INSTANCIA LAZYHOLDER");
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstance();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstance();
+        System.out.println(lazyHolder);
     }
 }
