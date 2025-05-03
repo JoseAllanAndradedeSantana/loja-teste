c5ccc82d90172fcbe6ffed723984671ce3127ab2
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2025-05-03 09:37:30 -0300

    padrao strategy

---
+++ src/estudos/padraodeprojeto/ComportamentoNormal.java
@@ -0,0 +1,8 @@
+package estudos.padraodeprojeto;
+
+public class ComportamentoNormal implements Comportamento{
+    @Override
+    public void mover() {
+        System.out.println("Comportamento normal");
+    }
+}
