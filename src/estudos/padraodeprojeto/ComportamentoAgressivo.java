fa78cbd8d4e625f006f124e69cef0bdd913ee0b0
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2025-05-03 09:37:47 -0300

    padrao strategy

---
+++ src/estudos/padraodeprojeto/ComportamentoAgressivo.java
@@ -0,0 +1,8 @@
+package estudos.padraodeprojeto;
+
+public class ComportamentoAgressivo implements Comportamento{
+    @Override
+    public void mover() {
+        System.out.println("Comportamento Agressivo");
+    }
+}
