1ae45467b49a058bc21ab7aecdfd3ab4110bb3ff
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2025-05-03 09:38:11 -0300

    padrao strategy

---
+++ src/estudos/padraodeprojeto/ComportamentoDefensivo.java
@@ -0,0 +1,8 @@
+package estudos.padraodeprojeto;
+
+public class ComportamentoDefensivo implements Comportamento{
+    @Override
+    public void mover() {
+        System.out.println("Comportamento Defensivo");
+    }
+}
